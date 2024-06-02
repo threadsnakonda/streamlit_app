ad1f496c3b21143d2ded4db5233cfec705527e00
threadsnakonda <96891584+threadsnakonda@users.noreply.github.com>
2024-06-02 22:15:39 +0900

    Add files via upload

---
+++ app_chat_gpt.py
@@ -0,0 +1,116 @@
+import streamlit as st
+from st_pages import Page, show_pages
+from openai import OpenAI
+
+class App_chat_gpt:
+
+    def __init__(self):
+
+        self.password = ''
+        if 'password_correct' not in st.session_state:
+            st.session_state.password_correct = False
+
+        if not 'api_key' in st.session_state:
+            st.session_state.api_key = False
+        self.init_chat = {
+            'role': 'system',
+            'content': '한국말로 간결하게 대답해줘. 코드를 생성하는 영역에는 영어로 써줘',
+            }
+
+        st.set_page_config(
+            page_title="Chat GPT",
+            page_icon="🚀",
+            layout="wide",
+            initial_sidebar_state="expanded",
+            )
+
+        show_pages(
+            [
+                Page('app_chat_gpt.py', '1. Chat_GPT'),
+                Page('other_pages/app_meeting_minutes.py', '2. Meeting Minutes'),
+            ]
+        )
+
+        self.main_ui()
+
+    def main_ui(self):
+
+        if not st.session_state.password_correct:
+            with st.form(key = 'form'):
+                password = st.text_input(label = 'Pass Word')
+                submit = st.form_submit_button(label = 'Submit')
+
+            if submit:
+                if not password == self.password:
+                    st.title(':no_entry_sign: :busts_in_silhouette: Access is not possible')
+                    return
+                else:
+                    st.session_state.password_correct = True
+                    st.rerun()
+            else:
+                return
+
+        if st.session_state.password_correct:
+
+            if not st.session_state.api_key:
+                st.session_state.api_key = st.text_input(label = 'OpenAI API KEY')
+
+            st.subheader('무엇이든 물어보살')
+
+            if 'chat_history' not in st.session_state:
+                st.session_state.chat_history = []
+
+            self.print_chat()
+            if question :=st.chat_input('메세지를 입력해 주세요'):
+                st.chat_message('user').write(question)
+                with st.chat_message('assistant'):
+                    answer_placeholder = st.empty()
+                self.chat_generator(question, answer_placeholder)
+
+            if st.session_state.chat_history:
+                del_chat_chain = st.button(label = '대화 삭제')
+                if del_chat_chain:
+                    self.delete_chat_chain()
+
+
+    def print_chat(self):
+        if 'chat_history' in st.session_state:
+            for chat in st.session_state.chat_history[1:]:
+                st.chat_message(chat['role']).write(chat['content'])
+
+
+    def chat_generator(self, question, answer_placeholder):
+
+        client = OpenAI(api_key = st.session_state.api_key)
+        
+        if not st.session_state.chat_history:
+            st.session_state.chat_history.append(self.init_chat)
+
+        self.add_chat_history('user', question)
+
+        response = client.chat.completions.create(
+            model = 'gpt-4o',
+            messages = st.session_state.chat_history,
+            stream = True,
+        )
+
+        answer = ''
+        for chunk in response:
+            chunk_content = chunk.choices[0].delta.content
+            if isinstance(chunk_content, str):
+                answer += chunk_content
+                answer_placeholder.write(answer)
+
+        self.add_chat_history('assistant', answer)
+
+    def add_chat_history(self, role, content):
+        st.session_state.chat_history.append(
+            {'role' : role, 'content' : content,}
+            )
+
+    def delete_chat_chain(self):
+        st.session_state.chat_history = []
+        st.rerun()
+
+if __name__ == '__main__':
+    App_chat_gpt()
---
+++ other_pages/app_meeting_minutes.py
@@ -0,0 +1,176 @@
+import streamlit as st
+import datetime
+from PIL import Image
+import io
+import pickle
+import os
+from pympler import asizeof
+
+class Meeting_Minutes:
+
+    def __init__(self):
+
+        self.today = datetime.datetime.today()
+        self.password = ''
+
+        if 'password_correct' not in st.session_state:
+            st.session_state.password_correct = False
+
+        if not 'load_contents' in st.session_state:
+            st.session_state.load_contents = True
+
+        st.set_page_config(
+            page_title="Meeting Minutes",
+            page_icon="🚀",
+            layout="wide",
+            initial_sidebar_state="expanded",
+            )
+
+        self.main_ui()
+        self.check_resouce()
+
+    def main_ui(self):
+
+        if not st.session_state.password_correct:
+            with st.form(key = 'form'):
+                password = st.text_input(label = 'Pass Word')
+                submit = st.form_submit_button(label = 'Submit')
+
+            if submit:
+                if not password == self.password:
+                    st.title(':no_entry_sign: :busts_in_silhouette: Access is not possible')
+                    return
+                else:
+                    st.session_state.password_correct = True
+                    st.rerun()
+            else:
+                return
+
+        if st.session_state.password_correct:
+
+            if os.path.exists('data') and st.session_state.load_contents:
+                saved_files = [file for file in os.listdir('data') if file.endswith('.pkl')]
+                if saved_files:
+                    contents_data_path = 'data/' + sorted([x for x in saved_files if 'contents' in x])[-1]
+                
+                    with open (contents_data_path, 'rb') as f:
+                        st.session_state.contents = pickle.load(f)
+                        st.session_state.load_contents = False
+
+            if not 'contents' in st.session_state:
+                st.session_state.contents = []
+
+            with st.form(key = 'form'):
+                date, title, byte_images, description = self.set_form()
+                submit = st.form_submit_button(label = ':orange[submit]')
+                
+            if submit:
+                if title != '' and description != '':
+                    self.add_contents(date, title, byte_images, description)
+
+            selected_idxs = list(range(0, len(st.session_state.contents)))
+            key_word = st.text_input('***:green[Search Key-word]***')
+            if key_word:
+                selected_idxs = self.find_idx(key_word)
+            if selected_idxs:
+                st.caption(selected_idxs)
+
+            save_col, empty_col1, empty_col2, download_col = st.columns(4)
+            with save_col:
+                save_digits = st.button('Save as binary digits')
+                if save_digits:
+                    with open (f'data/contents_{self.today.strftime("%Y_%m_%d_%H_%M_%S")}.pkl', 'wb') as f:
+                        pickle.dump(st.session_state.contents, f)
+
+            with download_col:
+                pickle_contents = pickle.dumps(st.session_state.contents)
+                st.download_button(
+                    label = 'Download as binary digits',
+                    data = pickle_contents,
+                    file_name = f'contents_{self.today.strftime("%Y_%m_%d_%H_%M_%S")}.pkl',
+                    mime = 'application/octet-stream',
+                )
+
+            with st.container(height = 500):
+                self.display_data(selected_idxs)
+
+
+    def set_form(self):
+        
+        title_col, image_col = st.columns(2)
+        with title_col:
+            date = st.date_input('***Date***', self.today)
+            title = st.text_input('***Title***', '')
+
+        with image_col:
+            images = st.file_uploader('***Image***',
+                                      accept_multiple_files = True,
+                                      type = [
+                                          'bmp', 'gif', 'ico', 'jpeg',
+                                          'jpg', 'png', 'tif', 'tiff',
+                                          'webp',])
+            
+        byte_images = []
+        if images:
+            for image in images:
+                byte_images.append(image.read())
+
+        description = st.text_area('***Description***', '')
+
+        return date, title, byte_images, description
+
+
+
+    def add_contents(self, date, title, byte_images, description):
+        content = {'date' : date, 'title' : title, 'images' : byte_images, 'description' : description}
+        st.session_state.contents.append(content)
+        st.session_state.contents.sort(key=lambda x: x['date'])
+
+    def find_idx(self, key_word):
+        selected_idxs = []
+        for idx, content in enumerate(st.session_state.contents):
+            if key_word.upper() in content['title'].upper() or key_word.upper() in content['description'].upper():
+                selected_idxs.append(idx)
+        return selected_idxs
+
+    def display_data(self, selected_idxs):
+        for idx, content in enumerate(st.session_state.contents):
+            if idx in selected_idxs:
+                with st.expander(f"***{idx}. {content['date']} : {content['title']}***", expanded = False):
+                    
+                    if content['images']:
+                        cols = st.columns(len(content['images']))
+                        for col, byte_image in zip(cols, content['images']):
+                            image = Image.open(io.BytesIO(byte_image))
+                            max_size = (200, 200)
+                            image.thumbnail(max_size)
+                            col.image(image)
+
+                    st.write(content['description'])
+
+                    delete = st.button(label = 'delete', key = f'{idx}')
+                    if delete:
+                        del st.session_state.contents[idx]
+                        st.rerun()
+
+    def format_size(self, size):
+        for unit in ['B', 'KB', 'MB', 'GB']:
+            if size < 1024:
+                return f"{size:.2f} {unit}"
+            size /= 1024
+        return f"{size:.2f} GB"
+
+    def check_resouce(self):
+        with st.sidebar:
+            st.text('<< used resource >>')
+            resource_dict = {}
+            for key in st.session_state.keys():
+                resource_dict[key] = asizeof.asizeof(st.session_state[key])
+            resource_dict = dict(sorted(resource_dict.items(), key=lambda item: item[1], reverse=True))
+            for key, value in resource_dict.items():
+                size = self.format_size(value)
+                if size.split(' ')[-1] in ['KB', 'MB', 'GB']:
+                    st.text(f'{key} : {size}')
+
+if __name__ == '__main__':
+    Meeting_Minutes()
